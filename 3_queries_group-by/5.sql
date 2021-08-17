0866fb571ed2e5b459569834d94c18aebd62f6a2
woodpeckershop <chris.sun5567@gmail.com>
2021-08-17 14:26:27 -0700

    add teacher database

--- 3_queries_group-by/5.sql
+++ 3_queries_group-by/5.sql
@@ -1,7 +1,8 @@
-SELECT students.name, AVG(assignment_submissions.duration), avg(assignments.duration)
-FROM students join assignment_submissions on student_id = students.id
-join assignments on assignment_id=assignments.id
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
 WHERE end_date IS NULL
-group by students.name
+GROUP BY student
 HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-order by AVG(assignment_submissions.duration)
+ORDER BY average_assignment_duration;
